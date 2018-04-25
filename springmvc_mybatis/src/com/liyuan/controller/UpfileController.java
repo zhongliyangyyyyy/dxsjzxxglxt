838bcc989e2104db02745dbec68798c1de0c4b58
zhongliyangyyyyy <37304098+zhongliyangyyyyy@users.noreply.github.com>
2018-04-25 15:18:50 +0800

    zly
    文件上传

--- springmvc_mybatis/src/com/liyuan/controller/UpfileController.java
+++ springmvc_mybatis/src/com/liyuan/controller/UpfileController.java
@@ -1,24 +1,92 @@
 ﻿package com.liyuan.controller;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URLEncoder;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import net.sf.json.JSONObject;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.liyuan.po.Upfile;
 import com.liyuan.service.UpfileService;
+import com.liyuan.utils.GyUtils;
 
 /**
  * 用户Controller*/
 @Controller
 public class UpfileController {
 
-	@Autowired
-	private UpfileService upfileService;
-	//登录
-	@RequestMapping("/upfile")
-	@ResponseBody public Upfile upfile() throws Exception{
-		System.out.println("sasaassasasa");
-		return upfileService.findById("0ceba96c-dba8-4581-896d-95032b70db53");
-	}	
+    /**  
+     * 文件上传功能  
+     * @param file  
+     * @return  
+     * @throws IOException   
+     */  
+    @RequestMapping(value="/upload",method=RequestMethod.POST)  
+    @ResponseBody  
+    public JSONObject upload(MultipartFile file,HttpServletRequest request) throws IOException{ 
+    	HttpSession session=request.getSession(true);
+    	String id=(String)session.getAttribute("id");
+    	String path="E:\\dxsjzxxglxt\\file\\"+id;
+        //String path = request.getSession().getServletContext().getRealPath("upload");  
+        String fileName = file.getOriginalFilename();
+        String suffix = fileName.substring(fileName.lastIndexOf(".") + 1);
+        String uuid=GyUtils.getUUid();
+        String uploadfilename=uuid+"."+suffix;
+        File dir = new File(path,uploadfilename);          
+        if(!dir.exists()){  
+            dir.mkdirs();  
+        }  
+        //MultipartFile自带的解析方法  
+        file.transferTo(dir);  
+        JSONObject result=new JSONObject();
+        result.put("success", true);
+        result.put("message", "上传文件成功");
+        result.put("url", path+uploadfilename);
+        return GyUtils.returnResult(true, "成功", result);  
+    }  
+      
+    /**  
+     * 文件下载功能  
+     * @param request  
+     * @param response  
+     * @throws Exception  
+     */  
+    @RequestMapping("/down")  
+    public void down(HttpServletRequest request,HttpServletResponse response) throws Exception{  
+        //模拟文件，myfile.txt为需要下载的文件  
+        String fileName = request.getSession().getServletContext().getRealPath("upload")+"/myfile.txt";  
+        //获取输入流  
+        InputStream bis = new BufferedInputStream(new FileInputStream(new File(fileName)));  
+        //假如以中文名下载的话  
+        String filename = "下载文件.txt";  
+        //转码，免得文件名中文乱码  
+        filename = URLEncoder.encode(filename,"UTF-8");  
+        //设置文件下载头  
+        response.addHeader("Content-Disposition", "attachment;filename=" + filename);    
+        //1.设置文件ContentType类型，这样设置，会自动判断下载文件类型    
+        response.setContentType("multipart/form-data");   
+        BufferedOutputStream out = new BufferedOutputStream(response.getOutputStream());  
+        int len = 0;  
+        while((len = bis.read()) != -1){  
+            out.write(len);  
+            out.flush();  
+        }  
+        out.close();  
+    }  
 }
+
